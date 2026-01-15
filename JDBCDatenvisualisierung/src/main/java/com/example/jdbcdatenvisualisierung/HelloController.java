cf0f5d3e20a64628a851215e00d129b6d8921dab
Julian Dietachmair <julian.dietachmair@gmail.com>
2026-01-15 11:34:14 +0100

    Second Commit

--- JDBCDatenvisualisierung/src/main/java/com/example/jdbcdatenvisualisierung/HelloController.java
+++ JDBCDatenvisualisierung/src/main/java/com/example/jdbcdatenvisualisierung/HelloController.java
@@ -1,14 +1,62 @@
 package com.example.jdbcdatenvisualisierung;
 
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
-import javafx.scene.control.Label;
+import javafx.scene.chart.ScatterChart;
+import javafx.scene.control.Slider;
+import java.sql.*;
 
 public class HelloController {
-    @FXML
-    private Label welcomeText;
 
-    @FXML
-    protected void onHelloButtonClick() {
-        welcomeText.setText("Welcome to JavaFX Application!");
+    public ScatterChart scatterChart;
+    public Slider populationSlider;
+
+    public void initialize() {
+
+        Connection conn = null;
+        Statement st = null;
+        ResultSet rs = null;
+
+        try {
+            conn = DriverManager.getConnection(
+                    "jdbc:postgresql://xserv:5432/world2",
+                    "reader",
+                    "reader"
+            );
+
+            st = conn.createStatement();
+
+
+            rs = st.executeQuery("""
+                SELECT c.name, c.lifeexpectancy, c.population
+                FROM country c
+            """);
+
+
+            while (rs.next()) {
+
+                String country = rs.getString("name");
+                double lifeExpectancy = rs.getDouble("lifeexpectancy");
+                long population = rs.getLong("population");
+
+                System.out.println(
+                        country + " | Lebenserwartung: "
+                                + lifeExpectancy + " | Bevölkerung: "
+                                + population
+                );
+            }
+
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        } finally {
+
+
+            try { if (rs != null) rs.close(); } catch (SQLException ignored) {}
+            try { if (st != null) st.close(); } catch (SQLException ignored) {}
+            try { if (conn != null) conn.close(); } catch (SQLException ignored) {}
+        }
+    }
+
+    public void onHelloButtonClick(ActionEvent actionEvent) {
     }
 }
--- JDBCDatenvisualisierung/src/main/java/module-info.java
+++ JDBCDatenvisualisierung/src/main/java/module-info.java
@@ -1,6 +1,7 @@
 module com.example.jdbcdatenvisualisierung {
     requires javafx.controls;
     requires javafx.fxml;
+    requires java.sql;
 
 
     opens com.example.jdbcdatenvisualisierung to javafx.fxml;
